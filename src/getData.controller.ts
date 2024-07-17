be8143c21be038e9b91c7a8393f15327ede170bf
LouisLee-Dev <louislee0996@gmail.com>
2024-07-17 09:54:40 -0700

    fix:get price

--- src/getData.controller.ts
+++ src/getData.controller.ts
@@ -3,7 +3,7 @@ import { Page } from "puppeteer";
 interface IProductData {
   id?: string;
   title?: string;
-  body_html?: string;
+  description?: any;
   images?: string[];
   vendor?: string;
   price?: any;
@@ -29,6 +29,9 @@ const getData = async (page: Page) => {
     };
   });
 
+  const description_element = await page.$('#container > div.root > div.layout-body > div.layout-left > div.module_title > div > h1');
+  const description =  description_element?.evaluate((el:any) => el.textContent.trim() || "");
+
   const product = async () => {
     await page.waitForSelector('#container > div.root > div.layout-body > div.layout-right > div > div > div > div.module_sku > div > div.sku-info > div:nth-child(2) > a:nth-child(1)', { timeout: 10000 });
     const module_dialog_element = await page.$('#container > div.root > div.layout-body > div.layout-right > div > div > div > div.module_sku > div > div.sku-info > div:nth-child(2) > a:nth-child(1)');
@@ -39,13 +42,15 @@ const getData = async (page: Page) => {
 
     const priceData = await sku_module_element?.evaluate((el: Element) => {
 
-      let priceItems:any;
+      let priceItems:{quality: any, price: any}[] | any = [];
 
       const price_mul_elements = el.querySelectorAll('.product-price .price-list .price-item');
       const price_simple_element = el.querySelector('.product-price .price-list .price-range span.price');
 
-      price_mul_elements && price_mul_elements !== undefined && price_mul_elements !== null && price_mul_elements.item(0) !== undefined && price_mul_elements.item(0) !== null && price_mul_elements.length < 1 ?
+      !price_simple_element ?         
+        // console.log(price_mul_elements): console.log(price_simple_element)
         price_mul_elements.forEach((element: Element) => {
+
           const qualityElement = element.querySelector('.quality')?.textContent?.trim() || "";
           const priceElement = element.querySelector('.price span')?.textContent?.trim() || "";
 
@@ -56,18 +61,18 @@ const getData = async (page: Page) => {
       return priceItems;
     });
 
-    const options = await sku_module_element?.evaluate((el: Element) => {      
+    const options = await sku_module_element?.evaluate((el: Element) => {
       const option1_elements = document.querySelectorAll('div.sku-info > div:nth-child(2) > a.image');
       const option1 = Array.from(option1_elements).map((option: any) => {
         option.click();
         const name = el.querySelector('div.sku-info > h4:nth-child(1) > span')?.innerHTML || "";
         const img = option.querySelector('img')?.getAttribute('src') || "";
-        return {name, img};
+        return { name, img };
       });
       const option1_name = el.querySelector('div.sku-info > h4:nth-child(1)')?.textContent?.trim() || "";
-      
+
       const option2_elements = document.querySelectorAll('div.sku-info > div:nth-child(4) > a');
-      
+
       const option2 = Array.from(option2_elements).map((option: any) => {
         return option.querySelector('span').textContent?.trim() || null
       });
@@ -79,16 +84,16 @@ const getData = async (page: Page) => {
         const price = option.querySelector('span.price')?.innerHTML || "";
         return { type, price };
       });
-      const option3_name = el.querySelector('div.sku-info > h4:nth-child(5)')?.textContent?.trim() || "";      
+      const option3_name = el.querySelector('div.sku-info > h4:nth-child(5)')?.textContent?.trim() || "";
 
-      return { 
-        option1: { name: option1_name, value: option1 }, 
-        option2: { name: option2_name, value: option2 }, 
-        option3: { name: option3_name, value: option3 } 
+      return {
+        option1: { name: option1_name, value: option1 },
+        option2: { name: option2_name, value: option2 },
+        option3: { name: option3_name, value: option3 }
       };
     });
 
-    const variants = options?.option1.value.map((op1, op1_index) => 
+    const variants = options?.option1.value.map((op1, op1_index) =>
       options.option2?.value?.map((op2, op2_index) => {
         return {
           title: `${op1.name} / ${op2}`,
@@ -100,7 +105,7 @@ const getData = async (page: Page) => {
           option3: options.option3,
           imgUrl: op1.img
         }
-    })
+      })
     )
 
     const vendorSelectors = ['.strong', '.logistic-item']
@@ -114,19 +119,20 @@ const getData = async (page: Page) => {
     return Array.from(images).map(img => img.getAttribute('src') || "");
   });
 
-  const productResult = await product();
-
-  console.log(productResult);
+  const productResult = await product();  
 
   const ProductData: IProductData = {
     title: header.title,
     vendor: productResult.vendor || undefined,
+    description: description,
     price: productResult.priceData,
     options: [productResult.options?.option1, productResult.options?.option2, productResult.options?.option3],
     variants: productResult.variants,
     images,
   };
-  
+
+  console.log(ProductData)
+
   return ProductData;
 };
 
